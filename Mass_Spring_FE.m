1d4f462125ba659ed3f2f17ef653b2a250d109ec
Eli690 <ali.mohammadi3506@yahoo.com>
2021-11-11 00:05:17 +0200

    Mass_spring_with Forward Euler

---
+++ Mass_Spring_FE.m
@@ -0,0 +1,29 @@
+% Special case implementation of a simple oscillator simulation using
+% first-order Forward Euler solver.
+omega= 2;
+T0 = 2*pi/omega;
+dt = T0/20;
+tk = 3*T0;
+N_t = floor(tk/dt);
+t = linspace(0, N_t*dt, N_t+1);
+u = zeros(N_t+1, 1);
+v = zeros(N_t+1, 1);
+% Initial condition
+X_0 = 2;
+u(1) = X_0;
+v(1) = 0;
+% The implementation of Forward Euler method 
+for n = 1:N_t
+    u(n+1) = u(n) + dt*v(n);
+    v(n+1) = v(n) - dt*omega^2*u(n);
+end
+%We called the function osc_energy in here to caculate U and K 
+[K,U]=osc_energy(u, v, omega);
+Total_energy=K+U;            %Sum of Sum of the kinetic and potential energy
+figure
+plot(t, Total_energy, 'm-');
+%legend('numerical', 'exact', 'Location','northwest');
+title('Development of total energy with the Forward Euler method ')
+ylabel('Sum of the kinetic and potential energy [K+U]')
+xlabel('Time [s]');
+% print('tmp', '-dpdf'); print('tmp', '-dpng');
